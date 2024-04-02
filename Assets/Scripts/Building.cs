3b8ac131b577242677d91468cd6948dcdcd427e0
T3CNEO <neoshon421@gmail.com>
2024-04-02 19:36:46 -0400

    help

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    public GameObject track;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(laser, out hit) && Input.GetMouseButton(0))
+        {
+            Debug.Log("fhj");
+            if (hit.collider)
+            { 
+                Instantiate(track, hit.rigidbody.position, transform.rotation);
+            }
+        }
     }
 }
