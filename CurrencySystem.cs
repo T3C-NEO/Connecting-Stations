6ca4e1fc181608539a23e6cb6dadda5e969063fe
cardriver655 <143759967+cardriver655@users.noreply.github.com>
2024-04-17 14:23:48 -0400

    a mostly working money counter
    I have finally got the money to count upward by using a mouse click input for now. The reset money script somewhat works, it resets the scene but not the money and I haven't figured out why yet.

--- CurrencySystem.cs
+++ CurrencySystem.cs
@@ -1,28 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CurrencySystem : MonoBehaviour
 {
-    pubic const string Money = "Money";
+    public int count;
+    public Text text;
 
-    public static int money = 0;
-    // Start is called before the first frame update
     void Start()
     {
-     	money = PlayerPrefs.GetInt("money");
+        count = PlayerPrefs.GetInt("amount");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.GetButtonDown("Fire1"))
+        {
+            count += 25;
+            PlayerPrefs.SetInt("amount", count);
+            
+        }
 
-    public static void UpdateMoney()
-    {
-	PlayerPrefs.SetInt("Money", money);
-	coins = PlayerPrefs.GetInt("Money");
-	PlayerPrefs.Save();
+        text.text = "" + count;
+
     }
 }
