beebb3a46c791e043276d4da87e0f33440923700
T3CNEO <neoshon421@gmail.com>
2024-05-06 02:17:23 -0400

    main menu added

---
+++ Assets/MENU.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU : MonoBehaviour
+{
+    public GameObject rules;
+    public GameObject rulesText;
+    public GameObject play;
+    public GameObject rule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowRules()
+    {
+        rules.SetActive(true);
+        rulesText.SetActive(true);
+        play.SetActive(false);
+        rule.SetActive(false);
+    }
+    public void HideRules()
+    {
+        rules.SetActive(false);
+        rulesText.SetActive(false);
+        play.SetActive(true);
+        rule.SetActive(true);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
